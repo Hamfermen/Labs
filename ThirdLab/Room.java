349f414045d9bd6d7b7b2166cc31c4170c0ac354
Hamfermen <75379938+Hamfermen@users.noreply.github.com>
2022-12-17 20:36:59 +0300

    Update Room.java

--- ThirdLab/Room.java
+++ ThirdLab/Room.java
@@ -10,80 +10,6 @@ public final class Room {
 
 		public static final double JPS = 10000000;
 		public static final int DELAY = 5000;
-		public static final int COUNT_OF_HOURS_IN_ONE_ITERATION = 1;
-		public static final int COUNT_OF_HOURS_IN_DAY = 24;
-		public static final int COUNT_OF_DAY_IN_MOUNTHS = 28;
-		public static final String[] mounths = { "Янраль", "Марель", "Июнль", "Сентобрь" };
-		public static final String[] seasons = { "Зима", "Весна", "Лето", "Осень" };
-	}
-
-	public class VoidSubstanceException extends Exception {
-		@Override
-		public String toString() {
-			return "Произошла ошибка и в комнате появилась абсоютная пустота";
-		}
-	}
-
-	public class TwoRoomExeption extends Exception {
-		@Override
-		public String toString() {
-			return "Невозможно создать более одной комнаты";
-		}
-	}
-
-	public class RoomColaps extends RuntimeException {
-		public RoomColaps(Throwable th) {
-			super("Конец света наступил", th);
-		}
-	}
-
-	public class TimerExeption extends Exception {
-		public TimerExeption() {
-			super("Невозможно переключится на 2000 год");
-		}
-	}
-
-	private final class RoomTimer {
-
-		private int hour;
-		private int day;
-		private int mounthNow;
-		private int seasonNow;
-		private int year;
-		private TemperatureCondition state;
-
-		private RoomTimer(int hour, int day, int mounthNow, int year) {
-			this.hour = hour;
-			this.day = day;
-			this.mounthNow = mounthNow;
-			seasonNow = mounthNow;
-			this.year = year;
-			state = seasonNow == 2 ? TemperatureCondition.WARM : TemperatureCondition.COLD;
-		}
-
-		private void timeUpdater() throws TimerExeption {
-			boolean check = year < 2000;
-			year += (((hour + RoomConsts.COUNT_OF_HOURS_IN_ONE_ITERATION) / RoomConsts.COUNT_OF_HOURS_IN_DAY + day)
-					/ RoomConsts.COUNT_OF_DAY_IN_MOUNTHS + mounthNow) / 4;
-			mounthNow = (((hour + RoomConsts.COUNT_OF_HOURS_IN_ONE_ITERATION) / RoomConsts.COUNT_OF_HOURS_IN_DAY + day)
-					/ RoomConsts.COUNT_OF_DAY_IN_MOUNTHS + mounthNow) % 4;
-			day = ((hour + RoomConsts.COUNT_OF_HOURS_IN_ONE_ITERATION) / RoomConsts.COUNT_OF_HOURS_IN_DAY + day)
-					% RoomConsts.COUNT_OF_DAY_IN_MOUNTHS;
-			hour = (hour + RoomConsts.COUNT_OF_HOURS_IN_ONE_ITERATION) % RoomConsts.COUNT_OF_HOURS_IN_DAY;
-			if (year == 2000 && check)
-				throw new TimerExeption();
-			state = seasonNow == 2 ? TemperatureCondition.WARM : TemperatureCondition.COLD;
-		}
-
-		public String getTime() {
-			return "Время: " + Integer.toString(hour) + ":00:00 День: " + Integer.toString(day) + " Месяц: "
-					+ RoomConsts.mounths[mounthNow] + " Сезон года: " + RoomConsts.seasons[seasonNow] + " Год: "
-					+ Integer.toString(year);
-		}
-		public TemperatureCondition getTemperatureState() {
-			return state;
-		}
-
 	}
 
 	private BodyOfRoom floor;
@@ -93,12 +19,7 @@ public final class Room {
 	private BodyOfRoom wall_4;
 	private BodyOfRoom ceiling;
 
-	private static int countOfRooms = 0;
-
 	private Karlson karlson;
-	private Malish malish;
-
-	private RoomTimer roomTimer;
 
 	private Workbench workbench;
 	private Fireplace fireplace;
@@ -106,24 +27,12 @@ public final class Room {
 
 	private List<Food> foodList;
 
-	public Room() throws TwoRoomExeption {
-		this(23, 27, 3, 1999);
-	}
-
-	public Room(int hour, int day, int mounthNow, int year) throws TwoRoomExeption {
-		try {
-			countOfRooms++;
-			if (countOfRooms > 1) throw new TwoRoomExeption();
-		}catch(TwoRoomExeption e) {
-			throw new RoomColaps(e);
-		}
-		fireplace = new Fireplace("Rock", 0.8, 8);
-		keroseneLamp = new KeroseneLamp("Silver", 0.1);
-		createFood();
+	public Room() {
+	fireplace = new Fireplace("Rock", 0.8, 8);
+	keroseneLamp = new KeroseneLamp("Silver", 0.1);
+	createFood();
     	generateRoom();
-    	roomTimer = new RoomTimer(hour, day, mounthNow, year);
     	karlson = Karlson.getKarlson();
-    	malish = Malish.getMalish();
     	try {
 			startSimulate();
 		} catch (InterruptedException e) {
@@ -168,7 +77,6 @@ public final class Room {
 	private void createFood() {
 		foodList = new ArrayList<>();
 		foodList.add(new Food("сладкое", "варенье", "выпил всё"));
-		foodList.add(new Food("печеные", "яблоки", "съел все"));
 	}
 
 	private void startSimulate() throws InterruptedException {
@@ -177,12 +85,8 @@ public final class Room {
 				roomUpdater();
 				try {
 					Thread.sleep(RoomConsts.DELAY);
-					tryCreateVoid();
-					roomTimer.timeUpdater();
 				} catch (InterruptedException e1) {
 					e1.printStackTrace();
-				} catch (TimerExeption | VoidSubstanceException e) {
-					throw new RoomColaps(e);
 				}
 			}
 		});
@@ -193,25 +97,14 @@ public final class Room {
 	}
 
 	public void roomUpdater() {
-		System.out.println(roomTimer.getTime());
 		karlson.exist(this);
-		malish.exist(this);
 		fireplace.dynamicExist();
 		keroseneLamp.dynamicExist();
 		System.out.println(
 				(LightCondition.LIGHT == fireplace.getLightState() || LightCondition.LIGHT == keroseneLamp.getLightState())
 						? "В комнате светло"
 						: "В комнате темно");
-		System.out.println(
-				(TemperatureCondition.WARM == fireplace.getTemperatureState() || TemperatureCondition.WARM == keroseneLamp.getTemperatureState() || TemperatureCondition.WARM == roomTimer.getTemperatureState())
-						? "В комнате тепло"
-						: "В комнате холодно");
 		System.out.println("----------------------------------");
-	}
-
-	private void tryCreateVoid() throws VoidSubstanceException {
-		if (Math.random() >= 0.9)
-			throw new VoidSubstanceException();
 	}
 
 	public BodyOfRoom getWall_1() {
